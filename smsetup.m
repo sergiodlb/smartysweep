d912bbc89ff305726134e7e842d6856da21d6c42
Sergio de la Barrera <sergio@phys.cmu.edu>
2018-08-01 15:54:54 -0400

    added some devices to the sample smsetup script for convenience and to keep these code blocks in a central location

--- smsetup.m
+++ smsetup.m
@@ -4,6 +4,92 @@ close all;
 instrreset;
 global smdata;
 
+% GPIB addresses
+GPIB_BOARD = 'ni';
+BOARD_NUM = 0;
+
+SR860_GPIB = [4,6];
+K2400_GPIB = [24];
+K2000_GPIB = 16;
+AMI430_COM = 'COM4';
+AMI430_BAUD = 115200;
+SeekatUNO_COM = 'COM5';
+SeekatUNO_BAUD= 9600;
+%LS372_GPIB = 12;
+
+labels = 'ABCD';
+
+%% add AMI430 magnet controller
+try
+    ind = smloadinst('AMI430', [], AMI430_COM, 'BaudRate', AMI430_BAUD);
+    
+    % open serial communication
+    smopen(ind);
+    smdata.inst(ind).name = 'AMI430'; 
+    
+    % add channels
+    smaddchannel('AMI430', 'FIELD', 'AMI430.B');
+    smaddchannel('AMI430', 'RATE1', 'AMI430.rate1');
+    smaddchannel('AMI430', 'RATE2', 'AMI430.rate2');
+    smaddchannel('AMI430', 'RAMP', 'AMI430.ramp');
+    smaddchannel('AMI430', 'PAUSE', 'AMI430.pause');
+    smaddchannel('AMI430', 'PSWCH', 'AMI430.pswitch');
+    smaddchannel('AMI430', 'STATE', 'AMI430.state');
+    
+catch err
+    fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n']);
+end
+
+%% Add QD 6000 to rack
+try
+    ind = smloadinst('QD6000',[], GPIB_BOARD, BOARD_NUM, QD6000_GPIB);
+    
+    smopen(ind);
+    smdata.inst(ind).name = 'PPMS'; 
+    
+    smaddchannel('PPMS', 'TEMP', 'PPMS.T', [0, Inf, Inf, 1]); % [min, max, rate_max, factor]
+    smaddchannel('PPMS', 'TRATE', 'PPMS.Trate', [0.01, 20, Inf, 1]);
+    smaddchannel('PPMS', 'DFIELD1SHOT', 'PPMS.B');
+    smaddchannel('PPMS', 'BRATE', 'PPMS.Brate', [10.3, 190.5, Inf, 1]);
+%     smaddchannel('PPMS', 'PFIELD1SHOT', 'PPMS.Pfield'); % persistent field; we don't typically use this
+    smaddchannel('PPMS', 'HELIUMLEVEL', 'PPMS.He');
+    smaddchannel('PPMS', 'STANDBY', 'PPMS.standby'); % puts PPMS in standby mode (to reduce He boil-off); set-only
+    
+catch err
+   fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n'])
+end
+
+%% add SR860 lock-ins
+n = 0;
+for GPIB = SR860_GPIB
+    try
+        n = n + 1;
+        ind = smloadinst('SR860', [], GPIB_BOARD, BOARD_NUM, GPIB);
+
+        % open GPIB communication
+        smopen(ind);
+        if length(SR860_GPIB) > 1
+            name = ['SR860', labels(n)];
+        else
+            name = 'SR860';
+        end
+        smdata.inst(ind).name = name;
+
+        % add channels
+        smaddchannel(name, 'X', [name, '.X']); % fixed spaces (unicode 32) issue by replacement (unicode 0) --> inst.channels(uint8(inst.channels) == 32) = char(0)
+        smaddchannel(name, 'Y', [name, '.Y']);
+        smaddchannel(name, 'R', [name, '.R']);
+        smaddchannel(name, 'THETA', [name, '.phase']);
+        smaddchannel(name, 'FREQ', [name, '.freq']);
+        smaddchannel(name, 'VREF', [name, '.V']);
+        smaddchannel(name, 'SCAL', [name, '.sensitivity']);
+        smaddchannel(name, 'TAU', [name, '.timeconstant']);
+
+    catch err
+       fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n']);
+    end
+end
+
 %% Add ZI HF2LI lock-in to rack
 try
     ind = smloadinst('ZIHF2LI');
@@ -39,4 +125,102 @@ try
     
 catch err
     fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n']);
+end
+
+%% add SeekatUNO 8-channel DC voltage source
+try
+    ind = smloadinst('SeekatUNO', [], SeekatUNO_COM, 'BaudRate', SeekatUNO_BAUD);
+
+    % open communication
+    smopen(ind) 
+    smdata.inst(ind).name = 'SeekatUNO'; 
+    
+    % add channels
+    smaddchannel('SeekatUNO', 'V1', 'Vdd');
+    smaddchannel('SeekatUNO', 'V2', 'Vg');
+    smaddchannel('SeekatUNO', 'V3', 'SeekatUNO.V3');
+    smaddchannel('SeekatUNO', 'V4', 'SeekatUNO.V4');
+    smaddchannel('SeekatUNO', 'V5', 'SeekatUNO.V5');
+    smaddchannel('SeekatUNO', 'V6', 'SeekatUNO.V6');
+    smaddchannel('SeekatUNO', 'V7', 'SeekatUNO.V7');
+    smaddchannel('SeekatUNO', 'V8', 'SeekatUNO.V8'); % limits appear to be +/- 10 V
+    
+    smset('Vdd', 0);
+    smset('Vg', 0);
+    smset('SeekatUNO.V3', 0);
+    smset('SeekatUNO.V4', 0);
+    smset('SeekatUNO.V5', 0);
+    smset('SeekatUNO.V6', 0);
+    smset('SeekatUNO.V7', 0);
+    smset('SeekatUNO.V8', 0);
+catch err 
+   fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n'])
+end
+
+%% loading K2400s
+n = 0;
+for GPIB = K2400_GPIB
+    try
+        n = n + 1;
+        ind = smloadinst('Keithley2400', [], GPIB_BOARD, BOARD_NUM, GPIB);
+        
+        % open GPIB communicationZI
+        smopen(ind);
+        if length(K2400_GPIB) > 1
+            name = ['K2400', labels(n)];
+        else
+            name = 'K2400';
+        end
+        smdata.inst(ind).name = name;
+
+        % add channels
+        smaddchannel(name, 'VOLT', [name, '.V']);
+        smaddchannel(name, 'CURRENT', [name, '.I']);
+        smaddchannel(name, 'COMPLIANCE', [name, '.compliance']);
+        smaddchannel(name, 'ISSOURCEVOLT', [name, '.sourceVolt']);
+        smaddchannel(name, 'OUTPUTON', [name, '.outputOn']);
+        smaddchannel(name, 'RESISTANCE', [name, '.R']);
+        smaddchannel(name, 'VRANGE', [name, '.range']);
+        
+        % ensure voltage sourcing mode, turn output ON
+        if ~cell2mat(smget([name, '.sourceVolt']))
+            smset([name, '.sourceVolt'], 1);
+        end
+        if ~cell2mat(smget([name, '.outputOn']))
+            smset([name, '.outputOn'], 1);
+        end
+    
+    catch err
+       fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n']);
+    end
+end
+
+%% add Keithley 2000 digital multimeter (using Keithley 2002 driver)
+try
+    ind = smloadinst('Keithley2002', [], GPIB_BOARD, BOARD_NUM, K2000_GPIB);
+     
+    % open GPIB communication
+    smopen(ind) 
+    smdata.inst(ind).name = 'K2000'; 
+    
+    % add channels
+    smaddchannel('K2000', 'VOLT', 'K2000.V', [-Inf, Inf, Inf, 1]);
+    
+catch err
+   fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n'])
+end
+
+%% add Keithley DMM6500 digital multimeter (using Keithley 2002 driver; device in SCPI 200 emulator mode)
+try
+    ind = smloadinst('Keithley2002', [], GPIB_BOARD, BOARD_NUM, K6500_GPIB);
+     
+    % open GPIB communication
+    smopen(ind) 
+    smdata.inst(ind).name = 'K6500'; 
+    
+    % add channels
+    smaddchannel('K6500', 'VOLT', 'K6500.V', [-Inf, Inf, Inf, 1]);
+    
+catch err
+   fprintf(['*ERROR* problem with connecting to device\n' err.identifier ': ' err.message '\n'])
 end
