3d5c77c2609c182b1f83fa8939e0c1ad67569e76
Sergio de la Barrera <sdlb@mit.edu>
2019-10-15 10:44:06 -0400

    fixed bug in volts2E_n_save() dealing with coexisting fname_format and data_directory options

--- volts2E_n_save.m
+++ volts2E_n_save.m
@@ -26,7 +26,7 @@ parse(parser, varargin{:});
 data_directory = parser.Results.data_directory;
 
 % generate data filename
-[~, fname_org] = readcol(fnum, 1, varargin{:});
+[~, fname_org] = readcol(fnum, 1, 'data_directory', data_directory, varargin{:});
 [path, name, ext] = fileparts(fname_org);
 nameext = [name, ext];
 fname = fullfile(path, sprintf(format, nameext));
@@ -38,8 +38,7 @@ fprintf(fid, '%-24s%s\n', '#Timestamp', data_header); % must match fixed width a
 fclose(fid);
 
 % convert gate voltages to E,n
-% voltages = readcol(fnum, [V1col,V2col], 'data_directory', data_directory);
-voltages = readcol(fnum, [V1col,V2col], varargin{:});
+voltages = readcol(fnum, [V1col,V2col], 'data_directory', data_directory, varargin{:});
 fields = volts2E_n(voltages(:,1), voltages(:,2), config, varargin{:});
 E = fields(:,1);
 n = fields(:,2);
